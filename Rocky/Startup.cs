b235e95adfee962e7d38644225d65cbb180a848f
maxtkachuk <max.tkachukk01@gmail.com>
2022-07-29 16:02:22 +0300

    Added Email confirmation

--- Rocky/Startup.cs
+++ Rocky/Startup.cs
@@ -3,12 +3,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Rocky.Data;
 using Rocky.Models;
+using Rocky.Utility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -36,6 +38,9 @@ namespace Rocky
                 .AddDefaultTokenProviders().AddDefaultUI()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            services.AddDistributedMemoryCache();
             services.AddHttpContextAccessor();
             services.AddSession(Options =>
             {
---
+++ Rocky/Utility/EmailSender.cs
@@ -0,0 +1,61 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Threading.Tasks;
+
+namespace Rocky.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            return Execute(email, subject, htmlMessage);
+        }
+
+        public async Task Execute(string email, string subject, string body)
+        {
+            MailjetClient client = new MailjetClient("9e869111271dfa8d66f1f5ec713aaaef", "1fd69c0e151d43556436b4690d70b075")
+            {
+                Version = ApiVersion.V3_1,
+            };
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }
+             .Property(Send.Messages, new JArray {
+         new JObject {
+          {
+           "From",
+           new JObject {
+            {"Email", "max.tkachuk228@ukr.net"},
+            {"Name", "Maksym"}
+           }
+          }, {
+           "To",
+           new JArray {
+            new JObject {
+             {
+              "Email",
+              email
+             }, {
+              "Name",
+              "DotNetMastery"
+             }
+            }
+           }
+          }, {
+           "Subject",
+           subject
+          }
+           ,{
+           "HTMLPart",
+          body
+          }
+         }
+             });
+            await client.PostAsync(request);
+        }
+    }
+}
